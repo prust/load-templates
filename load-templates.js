6b10a08262133549456263b4e6046209e69d2aa3
Peter Rust <peter@cornerstonenw.com>
2013-05-22 21:36:21 -0700

    Made nested template regex less flexible; fixed bugs

--- load-templates.js
+++ load-templates.js
@@ -7,7 +7,7 @@ else
   root.loadTemplates = loadTemplates;
 var join = is_node ? require('path').join : function(root, path) { return root + '/' + path; };
 
-var nested_regex = /\{\{(\{)?\s*(.+?)\.html\s*\}\}\}?/gi;
+var nested_regex = /\{\{ (.+?)\.html \}\}?/gi;
 var loading = 'LOADING...';
 
 // TODO: pull out the template-loader into a separate npm module
@@ -29,8 +29,9 @@ function loadTemplates(path, options, callback) {
 
     templates[path] = template;
 
-    var nested_templates = template.match(nested_regex);
+    var nested_templates = template.match(nested_regex) || [];
     nested_templates.forEach(function(path) {
+      path = path.replace('{{ ', '').replace(' }}', '');
       var separator_ix = path.indexOf(' ');
       if (separator_ix > -1)
         path = path.slice(separator_ix + 1);
@@ -38,33 +39,31 @@ function loadTemplates(path, options, callback) {
         loadTemplates(path, options, callback);
     });
 
-    for (var path in templates)
-      if (templates[path] == loading)
+    for (var tmpl_path in templates)
+      if (templates[tmpl_path] == loading)
         return;
     
     callback(null, templates);
   }
 
-  if (options.root)
-    path = join(options.root, path);
+  var full_path = options.root ? join(options.root, path) : path;
 
   if (is_node)
-    require('fs').readFile(path, 'utf-8', cb);
+    require('fs').readFile(full_path, 'utf-8', cb);
   else if (root.$)
-    $.ajax(path, ajaxSettings(cb));
+    $.ajax(ajaxSettings({'url': full_path}, cb));
   else
     throw new Error('When running in the browser, jQuery/Zepto is necessary to pull in templates via ajax');
 }
 
-function ajaxSettings(callback) {
-  return {
-    'success': function(data) {
-      callback(null, data);
-    },
-    'error': function(jqXHR, textStatus, errorThrown) {
-      callback(errorThrown);
-    }
-  }
+function ajaxSettings(opts, callback) {
+  opts.success = function(data) {
+    callback(null, data);
+  };
+  opts.error = function(jqXHR, textStatus, errorThrown) {
+    callback(errorThrown || jqXHR.status || textStatus);
+  };
+  return opts;
 }
 
 })(this);
